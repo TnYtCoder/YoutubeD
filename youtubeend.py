7c15bbb914d9f6bfcc441fda523af7ae44b52be4
TnYtCoder <115485810+TnYtCoder@users.noreply.github.com>
2023-07-02 00:51:12 +0530

    Delete youtubeend.py

--- youtubeend.py
+++
@@ -1,9 +0,0 @@
-from colorama import Fore , Back , Style
-import time as t 
-
-print("")
-print("")
-print(Fore.GREEN + "[>]  𝙔𝙤𝙪𝙧 𝙑𝙞𝙙𝙚𝙤 𝙃𝙖𝙫𝙚 𝘽𝙚𝙚𝙣 𝙎𝙖𝙫𝙚𝙙")
-print("")
-print("[>]  𝙏𝙝𝙖𝙣𝙠 𝙮𝙤𝙪 𝙛𝙤𝙧 𝙪𝙨𝙞𝙣𝙜")
-t.sleep(3)
